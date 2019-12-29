affa10c13865be59afa2000f1a029fd3b9e272e1
Zheng Tao <zhengtaolim@gmail.com>
2019-12-29 17:06:30 +0800

    Score added

--- Assets/Trigger.cs
+++ Assets/Trigger.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Trigger : MonoBehaviour
 {
+    public static int Score = 0;
+
+    public Text txt;
     private void OnTriggerEnter2D(Collider2D other)
     {
         Destroy(other.gameObject);
+        Score += 10;
+        txt.text = "Score: " + Score;
     }
 }
---
+++ Assets/UpdateScore.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UpdateScore : MonoBehaviour
+{
+    public TextMeshPro txt;
+    private void Update()
+    {
+        Debug.Log(txt.text);
+    }
+}
