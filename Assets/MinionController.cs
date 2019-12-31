cc8c90cdc85b84118c107c566d149eea0e55ae6e
Zheng Tao <zhengtaolim@gmail.com>
2019-12-31 17:43:25 +0800

    Added minions

--- Assets/MinionController.cs
+++ Assets/MinionController.cs
@@ -7,9 +7,9 @@ public class MinionController : MonoBehaviour
     // Start is called before the first frame update
     private Rigidbody2D rb;
     public float speed;
-    Vector2 moveInput;
-    Vector2 previousVector;
- 
+    private Vector2 moveInput;
+    float delay = 0;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -23,20 +23,32 @@ public class MinionController : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        ChangeOnColliding();
+        if (delay > 0)
+        { 
+            delay -= Time.deltaTime;
+        }
+        if (moveInput.x == 1)
+        {
+            this.transform.localScale = new Vector3(0.5f, 0.51f, 0.5f);
+        }
+        else if (moveInput.x == -1)
+        {
+            this.transform.localScale = new Vector3(-0.5f, 0.5f, 0.5f);
+        }
         Vector2 moveVelocity = moveInput.normalized * speed;
         rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.gameObject.tag == "Wall")
+        if (other.gameObject.tag == "Wall")
         {
+            if (delay > 0)
+                return;
+            delay = 0.00001f;
+
             float absX = Mathf.Abs(this.transform.position.x - other.transform.position.x);
             float absY = Mathf.Abs(this.transform.position.y - other.transform.position.y);
 
-            Debug.Log("X= " + absX + ", vecX= " + moveInput.x);
-            Debug.Log("Y= " + absY + ", vecY= " + moveInput.y);
-            Debug.Log(other.GetInstanceID());
             if (absY > absX)
             {
                 if (moveInput.y == 1)
@@ -64,12 +76,8 @@ public class MinionController : MonoBehaviour
                 moveInput.x = 0;
                 moveInput.y = 0;
             }
-
+           
         }
-    }
-    private void  ChangeOnColliding()
-    {
-     
     }
 
 }
--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NewBehaviourScript : MonoBehaviour
 {
@@ -10,7 +11,9 @@ public class NewBehaviourScript : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 moveVelocity;
     public float speed;
+    public int Score = 0;
 
+    public Text txt;
     // Update is called once per frame
     private void Start()
     {
@@ -21,16 +24,25 @@ public class NewBehaviourScript : MonoBehaviour
         Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         if (Input.GetAxisRaw("Horizontal") == 1)
         {
-            this.transform.localScale = new Vector3(0.5f, 0.51f, 0.5f);
+            this.transform.localScale = new Vector3(1f, 1f, 1f);
         }
         else if(Input.GetAxisRaw("Horizontal") == -1)
         {
-            this.transform.localScale = new Vector3(-0.5f, 0.5f, 0.5f);
+            this.transform.localScale = new Vector3(-1f, 1f, 1f);
         }
         moveVelocity = moveInput.normalized * speed;
     }
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Coin")
+        {
+            Destroy(other.gameObject);
+            Score += 10;
+            txt.text = "Score: " + Score;
+        }
     }
 }
