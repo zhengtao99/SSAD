7b0818e17b9ea37249415643e1ca32bda137ec6f
Zheng Tao <zhengtaolim@gmail.com>
2020-02-20 15:07:44 +0800

    Connection to database

---
+++ Assets/Scripts/ConnectionManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+namespace Assets.Scripts
+{
+     class ConnectionManager : MonoBehaviour
+    {
+        public static IEnumerator Login()
+        {
+            UnityWebRequest www = UnityWebRequest.Get("https://localhost:44365/api/loginapi/aaa/123");
+            yield return www.SendWebRequest();
+            if (www.isNetworkError || www.isHttpError)
+            {
+                //Debug.Log(www.error);
+            }
+            else
+            {
+                //Debug.Log(www.downloadHandler.text);
+
+                // Or retrieve results as binary data
+                byte[] results = www.downloadHandler.data;
+            }
+        }
+    }
+    
+}
