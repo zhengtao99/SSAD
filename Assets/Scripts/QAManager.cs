8bb8b7a26df77bddea79933439259c3e9b0a5304
xiaochaorenn <54619857+xiaochaorenn@users.noreply.github.com>
2020-02-27 18:28:46 +0800

    Question and Pop up update
    - Introduced new pop up, buttons and continue button
    - Button changes according to correct or wrong answers

--- Assets/Scripts/QAManager.cs
+++ Assets/Scripts/QAManager.cs
@@ -10,14 +10,20 @@ public class QAManager : MonoBehaviour
     public GameObject topRightButton;
     public GameObject bottomLeftButton;
     public GameObject bottomRightButton;
+    public GameObject continueButton;
+    public Button pressedButton;
     public Text qnText;
     private Text ansText = null;
+    private int answerID;
+    public static ColorBlock originalColors;
     // Start is called before the first frame update
     void Start()
     {
         if(transform.name.Replace("(Clone)", "") == "QuestionPopUpPage"){
             RetrievePossibleAnswers();
             RetrieveQuestion();
+            continueButton = GameObject.Find("Continue");
+            continueButton.SetActive(false);
         }
     }
 
@@ -35,16 +41,76 @@ public class QAManager : MonoBehaviour
             int index = Random.Range(0,buttons.Count); //Not inclusive of Max
             GameObject button = buttons[index];//Retrieve button from list
             ansText = button.GetComponentInChildren<Text>();
-            ansText.text = "I'm the random " + index;
+            ansText.text = "answer " + index; //Retrieve 1 possible answer
+            answerID = 2;//Retrieve answer ID;
             buttons.RemoveAt(index);
         }
     }
 
     void RetrieveQuestion(){
-        qnText.text = "Qn: " + "I am the retrieved question";
+        qnText.text = "Qn: " + "The question will be retrieved here. Loading....";
+    }
+
+    void DisableButtons()
+    {
+        GameObject[] buttons = GameObject.FindGameObjectsWithTag("AnswerButton");
+        foreach (GameObject button in buttons)
+        {
+            button.GetComponent<Button>().interactable = false;
+
+        }
+    }
+    void EnableButtons()
+    {
+        GameObject[] buttons = GameObject.FindGameObjectsWithTag("AnswerButton");
+        foreach (GameObject button in buttons)
+        {
+            button.GetComponent<Button>().interactable = true;
+            button.GetComponentInChildren<Text>().color = Color.black;
+            button.GetComponent<Button>().colors = originalColors; 
+
+        }
     }
 
     public void VerifyAnswer(){
+
+        bool isCorrect = (answerID == 1? true :false);
+        DisableButtons();
+        if (isCorrect)
+        {
+            TurnGreen();
+        }
+        else
+        {
+            TurnRed();
+        }
+        continueButton.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        EnableButtons();
+        continueButton.SetActive(false);
         ChestPopUp.Instance.ResumeGame();
+    }
+    
+    public void TurnRed()
+    {
+        originalColors = pressedButton.colors;
+        this.GetComponentInChildren<Text>().color = Color.white; 
+        ColorBlock colors = pressedButton.colors;
+        colors.selectedColor = Color.red;
+        colors.disabledColor = Color.red;
+        pressedButton.colors = colors;
+    }
+
+    public void TurnGreen()
+    {
+        this.GetComponentInChildren<Text>().color = Color.white;
+        originalColors = pressedButton.colors;
+        ColorBlock colors = pressedButton.colors;
+        colors.selectedColor = Color.green;
+        colors.disabledColor = Color.green;
+        pressedButton.colors = colors;
     }
 }
