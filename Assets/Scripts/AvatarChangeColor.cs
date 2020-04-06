c5977e7006478a24f78a5711372454fa7cc84b68
E180117 <48940683+E180117@users.noreply.github.com>
2020-04-06 21:18:35 +0800

    Leaderboard avatar ui changes
    Leaderboard avatar ui changes

--- Assets/Scripts/AvatarChangeColor.cs
+++ Assets/Scripts/AvatarChangeColor.cs
@@ -1,16 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AvatarChangeColor : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Color background = new Color(Random.Range(0f, 1f),Random.Range(0f, 1f),Random.Range(0f, 1f));
+        Image renderer = GetComponent<Image>();
 
-        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
-        renderer.color = background;
+        renderer.GetComponent<Image>().color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
     }
 
     // Update is called once per frame
